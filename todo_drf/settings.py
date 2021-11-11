4574f531565df9e080b4f0fc5e55c499faca7174
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2021-11-11 12:30:27 +0530

    Changes database to sqlite3

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'todo_drf.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-      'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #   'default': {
+    #     # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'test',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '123456789',
+    #     'HOST': 'localhost',
+    # }
 }
 
 
